0afc0b22bd1e3117dfe59dcb60ed69bcd8997cbb
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-01-28 14:52:36 +0100

    wip

--- Sources/Coenttb Vapor/Response.swift
+++ Sources/Coenttb Vapor/Response.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Vapor
+import Coenttb_Web_Models
 
 extension Response {
     public static func success<T: Codable>(
@@ -38,7 +39,7 @@ extension Response {
         message: String? = nil,
         status: HTTPStatus = .ok
     ) -> Response {
-        let response = Response.Envelope(success: success, data: data, message: message)
+        let response = Envelope(success: success, data: data, message: message)
         do {
             let encoder = JSONEncoder()
             encoder.dateEncodingStrategy = .iso8601
@@ -62,42 +63,6 @@ extension Response {
     ) -> Response {
         struct Empty: Codable {}
         return Self.json(success: success, data: Optional<Empty>.none, message: message, status: status)
-    }
-}
-
-extension Response {
-    public struct Envelope<T: Codable>: Codable {
-        public let success: Bool
-        public let data: T?
-        public let message: String?
-        public let timestamp: Date
-        
-        public init(success: Bool, data: T?, message: String?) {
-            self.success = success
-            self.data = data
-            self.message = message
-            self.timestamp = Date()
-        }
-        
-        enum CodingKeys: String, CodingKey {
-            case success, data, message, timestamp
-        }
-        
-        public func encode(to encoder: Encoder) throws {
-            var container = encoder.container(keyedBy: CodingKeys.self)
-            try container.encode(success, forKey: .success)
-            try container.encodeIfPresent(data, forKey: .data)
-            try container.encodeIfPresent(message, forKey: .message)
-            try container.encode(timestamp, forKey: .timestamp)
-        }
-        
-        public init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            success = try container.decode(Bool.self, forKey: .success)
-            data = try container.decodeIfPresent(T.self, forKey: .data)
-            message = try container.decodeIfPresent(String.self, forKey: .message)
-            timestamp = try container.decode(Date.self, forKey: .timestamp)
-        }
     }
 }
 
