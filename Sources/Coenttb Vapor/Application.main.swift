b8a34f88230cc9006abe2989abbed0d0c961b788
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-02-14 13:02:42 +0100

    wip

--- Sources/Coenttb Vapor/Application.main.swift
+++ Sources/Coenttb Vapor/Application.main.swift
@@ -21,57 +21,64 @@ extension Application {
         configure: (Application) async throws -> Void
     ) async throws {
         do {
-            try await withDependencies {
+            prepareDependencies {
                 $0.application = application
-            } operation: {
-                @Dependency(\.application) var application
-                
-                @Dependency(\.envVars.port) var port
-                application.http.server.configuration.port = port
-                
-                application.logger.info("Application starting with environment: \(environment)")
-                
-                application.middleware = .init()
-                
-                application.middleware.use(
-                    CORSMiddleware(configuration: corsMiddlewareConfiguration)
-                )
-                
-                application.middleware.use(ErrorMiddleware.default(environment: application.environment))
-                
-                application.middleware.use { request, next in
-                    let timer = RequestTimer()
-                    do {
-                        let response = try await next.respond(to: request)
-                        
-                        let message = [
-                            "\(response.status.code)",
-                            "\(timer.milliseconds)ms",
-                            "\(request.method) \(request.url.string)",
-                            "request-id=\(request.id)"
-                        ]
-                            .joined(separator: " | ")
-                        
-                        request.logger.log(
-                            level: .info,
-                            "\(message)"
-                        )
-                        
-                        return response
-                    } catch {
-                        request.logger.log(
-                            level: .error,
-                            "\(request.method) \(request.url.string) -> ERROR [\(timer.milliseconds)ms]: \(error)",
-                            metadata: ["request-id": .string(request.id)]
-                        )
-                        
-                        throw error
-                    }
-                }
-                
-                try await configure(application)
-                try await application.execute()
             }
+            
+            @Dependency(\.application) var application
+            
+            @Dependency(\.envVars.port) var port
+            application.http.server.configuration.port = port
+            
+            application.logger.info("Application starting with environment: \(environment)")
+            
+            application.middleware = .init()
+            
+            application.middleware.use(
+                CORSMiddleware(
+                    configuration: corsMiddlewareConfiguration
+                )
+            )
+            
+            application.middleware.use(
+                ErrorMiddleware.default(
+                    environment: application.environment
+                )
+            )
+            
+            application.middleware.use { request, next in
+                let timer = RequestTimer()
+                do {
+                    let response = try await next.respond(to: request)
+                    
+                    let message = [
+                        "\(response.status.code)",
+                        "\(timer.milliseconds)ms",
+                        "\(request.method) \(request.url.string)",
+                        "request-id=\(request.id)"
+                    ]
+                        .joined(separator: " | ")
+                    
+                    request.logger.log(
+                        level: .info,
+                        "\(message)"
+                    )
+                    
+                    return response
+                } catch {
+                    request.logger.log(
+                        level: .error,
+                        "\(request.method) \(request.url.string) -> ERROR [\(timer.milliseconds)ms]: \(error)",
+                        metadata: ["request-id": .string(request.id)]
+                    )
+                    
+                    throw error
+                }
+            }
+            
+            try await configure(application)
+            try await application.execute()
+            
         } catch {
             let logger = Logger(label: "CoenttbVapor.main")
             logger.error("Server.main level error: \(String(reflecting: error))")
