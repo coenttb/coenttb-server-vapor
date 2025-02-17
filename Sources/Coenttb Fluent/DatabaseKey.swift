feaa536edc31ab082f3d03193204ba7e715d6fee
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-02-17 10:30:14 +0100

    wip

---
+++ Sources/Coenttb Fluent/DatabaseKey.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  coenttb-server-vapor
+//
+//  Created by Coen ten Thije Boonkkamp on 17/02/2025.
+//
+
+import Foundation
+import Dependencies
+import Fluent
+import IssueReporting
+
+public enum DatabaseKey {}
+
+extension DatabaseKey: TestDependencyKey {
+    public static let testValue: (any Fluent.Database) = {
+        fatalError()
+    }()
+}
+
+extension DependencyValues {
+    public var database: (any Fluent.Database) {
+        get { self[DatabaseKey.self] }
+        set { self[DatabaseKey.self] = newValue }
+    }
+}
